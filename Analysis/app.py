bfeab0e936367d2e93e6f514ce4bf331b76eacbf
unknown <pfspring84@gmail.com>
2021-05-25 16:26:13 -0500

    additional changes for tmin, tmax, tavg > start date

--- Analysis/app.py
+++ Analysis/app.py
@@ -65,6 +65,7 @@ def precipitation():
 
     return jsonify(all_precipitation)
 
+
 @app.route("/api/v1.0/stations")
 def stations():
     # Create our session (link) from Python to the DB
@@ -80,6 +81,7 @@ def stations():
     all_stations = list(np.ravel(results))
 
     return jsonify(all_stations)
+
 
 @app.route("/api/v1.0/tobs")
 def tobs():
@@ -98,31 +100,57 @@ def tobs():
 
     return jsonify(all_tobs)
 
-@app.route("/api/v1.0/<startd>")
-def start_date(startd):
-    """Return the input start date"""
 
+@app.route("/api/v1.0/<start>")
+def start_date(start):
+  
     session = Session(engine)
 
     """Return a calculated list of TMIN, TMAX, and TAVG for all dates greater than or equal to the start date"""
     
-    resultslist = session.query(func.min(Measurement.tobs),\
-                                func.max(Measurement.tobs),\
-                                func.round(func.avg(Measurement.tobs),1)).\
-                                filter(Measurement.date >= startd).all()
+    results1 = session.query(func.min(Measurement.tobs)).\
+        filter(Measurement.date >= start).all()        
+    results2 = session.query(func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start).all() 
+    results3 = session.query(func.round(func.avg(Measurement.tobs),1)).\
+        filter(Measurement.date >= start).all()
 
     session.close()
     
-    # Convert list of tuples into normal list
-    all_func_tobs = list(np.ravel(resultslist))
+    # Create a dictionary to assign 'name tags' to the calculated list
+
+    func_dict = {}
+    func_dict["min temperature"] = results1
+    func_dict["max temperature"] = results2
+    func_dict["avg temperature"] = results3
     
-    return jsonify(all_func_tobs)
+    # all_func_values.append(func_dict)  
+    start_func_tobs = list(np.ravel(func_dict))
+    return jsonify(start_func_tobs)    
+   
     
 @app.route("/api/v1.0/<start>/<end>")
 def start_end_dates(start, end):
-    """Return the input start and end dates"""
 
-    return jsonify(start, end)
+    session = Session(engine)
+    
+    """Return a calculated list of TMIN, TMAX, and TAVG for all dates between the start/end dates"""
+    
+    resultslist = session.query(func.min(Measurement.tobs),\
+                                func.max(Measurement.tobs),\
+                                func.round(func.avg(Measurement.tobs),1)).\
+                                filter(Measurement.date >= start).\
+                                filter(Measurement.date <= end).all()
+    
+    session.close()
+    
+    # Convert list of tuples into normal list
+    date_range_func_tobs = list(np.ravel(resultslist))
+    
+    return jsonify(date_range_func_tobs)
+
+    """Return the input start and end dates"""
+    # return jsonify(start, end)
 
 if __name__ == '__main__':
     app.run(debug=True)
